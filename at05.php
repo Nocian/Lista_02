fb0348843c94e752769f4c5451126c1295bd471a
MayconF <mn.maycon@gmail.com>
2025-07-10 18:17:10 -0300

    at-05-06

---
+++ at05.php
@@ -0,0 +1,17 @@
+<?php 
+    
+    $valorInicial = (int)$_GET['valorInicial'];
+    $valorTaxa = (int)$_GET['valorTaxa'];
+
+    $valorFinal =($valorInicial * $valorTaxa / 100) + $valorInicial;
+
+ 
+    echo "<p>Você o valor final é  : <strong>R$: " . $valorFinal . "</strong></p>";
+
+  
+
+?>
+<?php // FECHA O PHP ?>
+
+<p><a href="At05.html">Voltar ao formulário</a></p>
+
---
+++ at06.php
@@ -0,0 +1,18 @@
+<?php 
+    
+    $cidade = (string)$_GET['cidade'];
+    $bairro = (string)$_GET['bairro'];
+    $numeroCasa = (string)$_GET['numeroCasa'];
+    $quadra = (string)$_GET['quadra'];
+
+ 
+    echo "<p>Endereço:" . $cidade .  ", " . $bairro . ", " . $numeroCasa . ", Quadra: " . $quadra . "</p>";
+
+  
+
+?>
+<?php // FECHA O PHP ?>
+
+<p><a href="At06.html">Voltar ao formulário</a></p>
+<p><a href="Lista_02">Voltar à pasta</a></p>
+
